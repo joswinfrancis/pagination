b5eb229ca0ee0b354c39e428ea23f6932d9369b0
Joswin Francis <joswinfrancis2001@gmail.com>
2024-04-17 14:10:09 +0530

    pagination

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
 from .models import Data
 from django.db.models import Q
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def index(request):
-    if 'q' in request.GET:
-        q = request.GET['q']
-        # data = Data.objects.filter(last_name__icontains=q)
-        multiple_q = Q(Q(first_name__icontains=q) | Q(last_name__icontains=q))
-        data = Data.objects.filter(multiple_q)
-    else:
-        data = Data.objects.all()
+    data = Data.objects.all()
+    page = Paginator(data , 3)
+    page_list = request.GET.get('page')
+    page = page.get_page(page_list)
     context = {
-        'data': data
+        'page': page
     }
-    return render(request, 'dashboard/index.html', context)
+    return render(request, 'index.html', context)
